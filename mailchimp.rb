a8c5450eb367d2e0679f2a3743a9d64af7edd879
John Dyer <john@krumpt.com>
2011-09-01 15:57:01 -0300

    Edited mailchimp.rb via GitHub

--- mailchimp.rb
+++ mailchimp.rb
@@ -1,31 +1,32 @@
-require 'uri'
-require 'net/http'
-require 'json'
+%w(uri net/http json).each{|lib| require lib}
 
-message = $currentCall.initialText
-callerID = $currentCall.callerID
+#CONFIG
+YOUR_NAME = "Kevin Bond"
+API_URL = "http://us2.api.mailchimp.com/1.3"
+API_KEY = 'mailchimp_api_key'
+FROM_ADDRESS = "kbond@voxeo.com"
 
-apiKey = 'mailchimp's API_KEY'
+# Get the initial Text, and callerId
+initial_text = $currentCall.initialText
+caller_id = $currentCall.callerID
 
-id =Net::HTTP.get_response(URI.parse("http://us2.api.mailchimp.com/1.3/?method=listsForEmail&apikey=1a4d654d423a919183d00f8afdb8a558-us2&email_address=kbond@voxeo.com"))
+#Encode user data for API request
+to = URI.encode(YOUR_NAME)
+
+id =Net::HTTP.get_response(URI.parse("#{API_URL}/?method=listsForEmail&apikey=#{API_KEY}&email_address=#{FROM_ADDRESS}"))
 listID = JSON.parse(id.body)
 
-subject = URI.encode("You just recieved a text message!")
-message = URI.encode("#{message}")
+subject = URI.encode "You just recieved a text message!"
 
-email = "kbond@voxeo.com"
-from = URI.encode("#{callerID}")
-to = URI.encode("Kevin Bond")
+content = "[text]=#{URI.encode(initial_text)}"
+options = "&options[list_id]=#{listID[0]}&options[subject]=#{subject}&options[from_email]=#{FROM_ADDRESS}&options[from_name]=#{URI.encode(caller_id)}&options[to_name]=#{to}"
+parameters = "apikey=#{API_KEY}&type=plaintext&options=#{options}&content#{content}"
 
-content = "[text]=#{message}"
-options = "&options[list_id]=#{listID[0]}&options[subject]=#{subject}&options[from_email]=#{email}&options[from_name]=#{from}&options[to_name]=#{to}"
-parameters = "apikey=#{apiKey}&type=plaintext&options=#{options}&content#{content}"
-
-id = Net::HTTP.get_response(URI.parse("http://us2.api.mailchimp.com/1.3/?method=campaignCreate&#{parameters}"))
+id = Net::HTTP.get_response(URI.parse("#{API_URL}/?method=campaignCreate&#{parameters}"))
 cid = id.body[1..10]
 
-url = URI.parse("http://us2.api.mailchimp.com/1.3/?method=campaignSendNow&apikey=#{apiKey}&cid=#{cid}")
-req = Net::HTTP::Post.new("http://us2.api.mailchimp.com/1.3/?method=campaignSendNow&apikey=#{apiKey}&cid=#{cid}")
+url = URI.parse("#{API_URL}/?method=campaignSendNow&apikey=#{API_KEY}&cid=#{cid}")
+req = Net::HTTP::Post.new("#{API_URL}/?method=campaignSendNow&apikey=#{API_KEY}&cid=#{cid}")
 res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
 
 
