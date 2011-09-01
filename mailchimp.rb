ea4e19099f718716f2d6452c697874d39114698f
Kevin Bond <kbond@voxeo.com>
2011-09-01 15:51:22 -0300

    Edited mailchimp.rb via GitHub

--- mailchimp.rb
+++ mailchimp.rb
@@ -3,22 +3,22 @@ require 'net/http'
 require 'json'
 
 message = $currentCall.initialText
-callerID = $currentCall.callerID
+caller_id = $currentCall.callerID
 
 API_KEY = 'mailchimp's API_KEY'
 
 id =Net::HTTP.get_response(URI.parse("http://us2.api.mailchimp.com/1.3/?method=listsForEmail&apikey=#{API_KEY}&email_address=kbond@voxeo.com"))
-listID = JSON.parse(id.body)
+list_id = JSON.parse(id.body)
 
 subject = URI.encode("You just recieved a text message!")
 message = URI.encode("#{message}")
 
 email = "kbond@voxeo.com"
-from = URI.encode("#{callerID}")
+from = URI.encode("#{caller_id}")
 to = URI.encode("Kevin Bond")
 
 content = "[text]=#{message}"
-options = "&options[list_id]=#{listID[0]}&options[subject]=#{subject}&options[from_email]=#{email}&options[from_name]=#{from}&options[to_name]=#{to}"
+options = "&options[list_id]=#{list_id[0]}&options[subject]=#{subject}&options[from_email]=#{email}&options[from_name]=#{from}&options[to_name]=#{to}"
 parameters = "apikey=#{API_KEY}&type=plaintext&options=#{options}&content#{content}"
 
 id = Net::HTTP.get_response(URI.parse("http://us2.api.mailchimp.com/1.3/?method=campaignCreate&#{parameters}"))
